1b4d89e5f79450f380425b3885bed7543f9cd099
Kamran Karimov <58477876+Kamran8633@users.noreply.github.com>
2021-03-22 22:57:11 +0400

    added results page

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,36 @@ from src.forms import RegistrationForm, LoginForm, SearchForm
 from src.models import User
 
 
+results = [
+    {
+        'title': 'Laptop Stand for desk, Detachable Laptop Riser Notebook Holder Stand Ergonomic Aluminum Laptop '
+                 'Mount Computer Stand, Compatible with MacBook Air Pro, Dell XPS, Lenovo More 10-18" Laptops  ',
+        'price': '$22.99',
+        'rating': '4.6/5',
+        'url': 'https://www.amazon.com/Detachable-Notebook-Ergonomic-Aluminum-Compatible/dp/B08DD82FRR/ref=sr_1_3'
+               '?dchild=1&keywords=notebook+stand&qid=1616432225&sr=8-3',
+        'short_url': 'https://www.amazon.com/'
+    },
+    {
+        'title': 'Global ROM Oneplus 8 5G Mobile Phone 12GB 256GB /8GB 128GB 6.55" 90Hz Snapdragon 865 48MP 30W '
+                 '4300mAh NFC 5G Smartphone',
+        'price': '$485.56 - 559.56',
+        'rating': '4.6/5',
+        'url': 'https://www.aliexpress.com/item/4001263945664.html?spm=a2g0o.productlist.0.0.4e222876npEmQo&algo_pvid'
+               '=44651afa-e2e7-48a1-b206-6d7e416144d5&algo_expid=44651afa-e2e7-48a1-b206-6d7e416144d5-0&btsid'
+               '=0bb0623a16164323935431321ec84e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+        'short_url': 'https://www.aliexpress.com/'
+    },
+    {
+        'title': 'Monitor “Acer EK220Q”',
+        'price': '185AZN',
+        'rating': '0',
+        'url': 'https://tap.az/elanlar/elektronika/komputer-avadanliqi/21120675',
+        'short_url': 'https://www.tap.az/'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -18,7 +48,7 @@ def about():
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     form = SearchForm()
-    return render_template('search.html', title='Search', form=form)
+    return render_template('search.html', title='Search', form=form, res=results)
 
 
 @app.route("/register", methods=['GET', 'POST'])
