1bd7a18a41fc35b516953343dadbf2234f018a63
Kamran Karimov <kamran.karimov.std@bhos.edu.az>
2021-05-03 15:34:11 +0400

    added custom options feature for Distributor

--- src/extractor/__init__.py
+++ src/extractor/__init__.py
@@ -1,3 +1,9 @@
-from src.extractor.distributor import Distributor
+from src.extractor.distributor import Distributor, DistributorOptions
 
 distributor = Distributor()
+
+# options = DistributorOptions()
+# options.add_argument('amazon')
+# options.add_argument('tapaz')
+#
+# distributor_2 = Distributor(options=options)
--- src/extractor/distributor.py
+++ src/extractor/distributor.py
@@ -3,56 +3,111 @@ from src.extractor.scrape import AbstractScraper, Scrape_aliexpress, Scrape_tapa
 from src.extractor.driver import Driver
 import time
 
+webdriver = Driver(headless=True)
 
-webdriver = Driver(False)
+amazon_scraper = Scrape_amazon(driver=webdriver)
+tapaz_scraper = Scrape_tapaz(driver=webdriver, timeout=0.4)
+aliexpress_scraper = Scrape_aliexpress()
 
-amazon_scraper: AbstractScraper = Scrape_amazon(driver=webdriver)
-tapaz_scraper: AbstractScraper = Scrape_tapaz(driver=webdriver, timeout=0.4)
-aliexpress_scraper: AbstractScraper = Scrape_aliexpress()
+sorter = Sort()
+filterer = Filter()
 
-sorter: OptionHandlerInterface = Sort()
-filterer: OptionHandlerInterface = Filter()
+
+class DistributorOptions(object):
+    def __init__(self):
+        self._arguments = []
+        self._possible_arguments = ['tapaz', 'amazon', 'aliexpress']
+
+    def add_argument(self, argument):  # sourcery skip: remove-redundant-if
+        """
+        Adds an argument to the list
+
+        :Args:
+         - Sets the arguments
+        """
+        if argument in self._possible_arguments:
+            self._arguments.append(argument)
+        elif argument not in self._possible_arguments:
+            raise ValueError("no such argument")
+        else:
+            raise ValueError("argument can not be null")
+
+    @property
+    def arguments(self):
+        """
+        Returns a list of arguments needed for the distributor
+        """
+        return self._arguments
 
 
 class Distributor:
-    def __init__(self):
-        self._scraper_aliexpress = aliexpress_scraper
-        self._scraper_amazon = amazon_scraper
-        self._scraper_tapaz = tapaz_scraper
-        self._sorter = sorter
-        self._filterer = filterer
+    __filterer: OptionHandlerInterface
+    __sorter: OptionHandlerInterface
+    __scraper_aliexpress: AbstractScraper
+    __scraper_amazon: AbstractScraper
+    __scraper_tapaz: AbstractScraper
 
-        self._possible_websites = {'tapaz': self._scraper_tapaz, 'amazon': self._scraper_amazon,
-                                   'aliexpress': self._scraper_aliexpress}
-        self._full_api = {'tapaz': {}, 'amazon': {}, 'aliexpress': {}}
+    def __init__(self, options: DistributorOptions = None):
+        self.__sorter = sorter
+        self.__filterer = filterer
+
+        if options:
+            self.__argument_wrapper(options)
+        else:
+            self.__argument_wrapper()
+        self.__full_api = {'tapaz': {}, 'amazon': {}, 'aliexpress': {}}
+
+    def __argument_wrapper(self, options=None):
+        if options:
+            self.__possible_websites = {}
+
+            if 'tapaz' in options.arguments:
+                self.__scraper_tapaz = tapaz_scraper
+                self.__possible_websites.update({'tapaz': self.__scraper_tapaz})
+
+            if 'amazon' in options.arguments:
+                self.__scraper_amazon = amazon_scraper
+                self.__possible_websites.update({'amazon': self.__scraper_amazon})
+
+            if 'aliexpress' in options.arguments:
+                self.__scraper_aliexpress = aliexpress_scraper
+                self.__possible_websites.update({'aliexpress': self.__scraper_aliexpress})
+
+        else:
+            self.__scraper_tapaz = tapaz_scraper
+            self.__scraper_amazon = amazon_scraper
+            self.__scraper_aliexpress = aliexpress_scraper
+
+            self.__possible_websites = {'tapaz': self.__scraper_tapaz, 'amazon': self.__scraper_amazon,
+                                        'aliexpress': self.__scraper_aliexpress}
 
     def __call__(self, **kwargs):
-        self._websites: list = kwargs['websites']
+        self.__websites: list = kwargs['websites']
         time_start = time.time()
         total_product_num = 0
 
-        for website, iterscraper in self._possible_websites.items():
-            if website in self._websites:
+        for website, iterscraper in self.__possible_websites.items():
+            if website in self.__websites:
                 scraped_data = iterscraper(item=kwargs['item'], mode=kwargs['mode'])
 
-                self._sorter.define_options(sort_price_option='ascending', sort_rating_option='default')
-                self._filterer.define_options(currency='AZN', min_price=12, max_price=1200)
+                self.__sorter.define_options(sort_price_option='ascending', sort_rating_option='default')
+                self.__filterer.define_options(currency='AZN', min_price=12, max_price=1200)
 
-                handler: OptionHandler = OptionHandler(sort_handler=self._sorter, filter_handler=self._filterer)
+                handler: OptionHandler = OptionHandler(sort_handler=self.__sorter, filter_handler=self.__filterer)
                 handled = handler.operation(scraped_data)
 
-                self._full_api.update({website: handled})
+                self.__full_api.update({website: handled})
                 total_product_num += handled['details']['total_num']
 
         time_end = time.time()
 
-        self._full_api.update({
+        self.__full_api.update({
             'details': {
                 'exec_time': round(time_end - time_start, 2),
                 'total_num': total_product_num
             }
         })
 
-        print(f'GET request: {kwargs}\nRESPONSE: {self._full_api}')
+        print(f'GET request: {kwargs}\nRESPONSE: {self.__full_api}')
 
-        return self._full_api
+        return self.__full_api
